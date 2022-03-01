ff074c6a0762e98486a38392f4857b896fc1e5c0
joe welch <jwelch19@outlook.com>
2022-02-28 21:03:51 -0500

    commit tree 33675b26e85736538c4e1d680bd6fa89c5763c5d

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+  if (someValue <=199) {
+    result = 'This one is on me!';
+  }
+  else if (someValue <=2500) {
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else if (someValue > 2500) {
+    result = 'No can do.'
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result
+  if (city === 'NYC') {
+    result = 'Ok, sounds good.'
+  } else if (city === 'Pittsburgh') {
+    result = 'No go.'
+  }
+  return result 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result 
+  if (tip === 'generous') {
+    result = 'Thank you so much.'
+  }
+  else if (tip === 'not as generous'){
+    result = 'Thank you.'
+  }
+  else if (tip === 'thanks for everything'){
+    result = "Bye."
+  }
+  return result
 }
